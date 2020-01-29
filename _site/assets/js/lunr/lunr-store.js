fda342c66ab7693933d28336a7ae0a055bbce346
JangSeungHyun <ashton2.3jang@gmail.com>
2020-01-29 17:54:19 +0900

    불필요 파일 제거

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Nodejs 배포하기 1편 - Jenkins, Git 설정하기",
-        "excerpt":"Step0. 참고사이트 SpringBoot2로 Rest api 만들기(13) – Jenkins 배포(Deploy) + Git Tag Rollback Step1. Jenkins → 새로운 Item Enter an item name : 프로젝트 네임 Freesttyle project 선택 Step2. 프로젝트 구성 General → 이 빌드는 매개변수가 있습니다. Git Parameter 추가 Name : ROLLBACK_TAGS 아래 소스코드 관리에서 Git 추가할 때 파라미터...","categories": ["nodejs"],
-        "tags": ["nodejs","jenkins","git","pm2"],
-        "url": "http://localhost:4000/mylogs/nodejs/nodejs_deploy_1/",
-        "teaser":null}]
